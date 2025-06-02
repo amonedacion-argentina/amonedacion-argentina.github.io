3edacdb92650c16c40deeb77bd0b65cda205a361
Amonedación Argentina <info@amonedacion-argentina.com>
2025-06-02 20:20:29 -0300

    Update render.js

--- album2/js/render.js
+++ album2/js/render.js
@@ -1,5 +1,3 @@
-const CONTRACT_ADDRESS = '0x199f5418551db3afa002470c11c2f7eba5154a43';
-
 // Renderizar NFTs en el contenedor
 async function renderNFTs(tokenIds) {
     const container = document.getElementById('nfts-container');
