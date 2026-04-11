382020334b2fba85b4be9d1a7f1e997ba9011023
Amonedación Argentina <info@amonedacion-argentina.com>
2026-04-11 15:33:46 -0300

    Add files via upload

--- album/js/script.js
+++ album/js/script.js
@@ -7,7 +7,7 @@ const App = {
             'https://gateway.pinata.cloud/ipfs/',
             'https://dweb.link/ipfs/'
         ],
-        IPFS_HASH: 'QmVuKgQPv9dr2tkw2AHNAmnHSuq6PArGZ5AZ5gFSxmSRRg',
+        IPFS_HASH: 'QmNepCLvrFryPgqSLFNv6sZZwjviv59LrVCJD3TTQoZiMD',
         OPENSEA_URL: 'https://opensea.io/assets/ethereum/',
         RARIBLE_URL: 'https://rarible.com/ethereum/items/',
         OKX_URL: 'https://web3.okx.com/es-es/nft/asset/eth/',
